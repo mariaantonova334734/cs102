b3af07ce84b891bcd3e27ffa3adea413a5203911
Maria Antonova <90193542+mariaantonova334734@users.noreply.github.com>
2021-10-17 16:08:29 +0300

    Update caesar.go

--- homework01-go/caesar/caesar.go
+++ homework01-go/caesar/caesar.go
@@ -1,17 +1,72 @@
-package caesar
 
-func EncryptCaesar(plaintext string, shift int) string {
-	var ciphertext string
+import typing as tp
 
-	// PUT YOUR CODE HERE
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+"""
+Encrypts plaintext using a Caesar cipher.
+
+>>> encrypt_caesar("PYTHON")
+'SBWKRQ'
+>>> encrypt_caesar("python")
+'sbwkrq'
+>>> encrypt_caesar("Python3.6")
+'Sbwkrq3.6'
+>>> encrypt_caesar("")
+''
+"""
+	ciphertext = ""
+	for i in plaintext:
+		if i.isalpha():
+			if i.isupper():
+				if ord(i)+shift%26 > 90: 
+					ciphertext += chr(ord(i)+shift%26-26)
+				else:
+					ciphertext += chr(ord(i)+shift%26)
+			elif i.islower():
+				if ord(i)+shift%26 > 122:
+					ciphertext += chr(ord(i)+shift%26-26)
+				else:
+					ciphertext += chr(ord(i)+shift%26) 
+		else:
+			ciphertext += i
 
 	return ciphertext
-}
 
-func DecryptCaesar(ciphertext string, shift int) string {
-	var plaintext string
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+"""
+Decrypts a ciphertext using a Caesar cipher.
 
-	// PUT YOUR CODE HERE
-
+>>> decrypt_caesar("SBWKRQ")
+'PYTHON'
+>>> decrypt_caesar("sbwkrq")
+'python'
+>>> decrypt_caesar("Sbwkrq3.6")
+'Python3.6'
+>>> decrypt_caesar("")
+''
+"""
+plaintext = ""
+	for i in ciphertext:
+		if i.isalpha():
+			if i.isupper():
+				if ord(i)-shift%26 < 65: 
+					plaintext += chr(ord(i)-shift%26+26)
+				else:
+					plaintext += chr(ord(i)-shift%26)
+			elif i.islower():
+				if ord(i)-shift%26 < 97:
+					plaintext += chr(ord(i)-shift%26+26)
+				else:
+					plaintext += chr(ord(i)-shift%26) 
+		else:
+			plaintext += i
 	return plaintext
-}
+
+def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
+"""
+Brute force breaking a Caesar cipher.
+"""
+best_shift = 0
+# PUT YOUR CODE HERE
+return best_shift
+
