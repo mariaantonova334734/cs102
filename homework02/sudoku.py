4c0afbf1a12140d35dc0ce59da9a1c55b3e73f7c
Maria Antonova <90193542+mariaantonova334734@users.noreply.github.com>
2021-11-18 23:31:08 +0300

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -47,7 +47,7 @@ def display(grid: tp.List[tp.List[str]]) -> None:
     for row in range(9):
         print(
             "".join(
-                 grid[row][col].center(width) + ("|" if str(col) in "25" else "") for col in range(9)
+                grid[row][col].center(width) + ("|" if str(col) in "25" else "") for col in range(9)
             )
         )
         if str(row) in "25":
