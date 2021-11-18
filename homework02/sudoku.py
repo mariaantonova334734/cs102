7a04e10102c4fc1c32631e0a51b4b13b8ee6ccfb
Maria Antonova <90193542+mariaantonova334734@users.noreply.github.com>
2021-11-18 23:04:27 +0300

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -155,26 +155,29 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
 
     >>> grid = read_sudoku('puzzle1.txt')
     >>> solve(grid)
-    [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
+    [['5', '3', '4', '6', '7', '8', '9', '1    a = find_possible_values(grid, (x, y))
+        for i in a:
+            grid[x][y] = i
+            answer = solve(grid)
+            if answer is not None:
+                return answer
+        grid[x][y] = "."
+        return None', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
     emptypositions = find_empty_positions(grid)
     if emptypositions is None:
-        x = None
-        y = None
+        return grid
     else:
-
         x = emptypositions[0]
         y = emptypositions[1]
-    if (x is None) and (y is None):
-        return grid
-    a = find_possible_values(grid, (x, y))
-    for i in a:
-        grid[x][y] = i
-        answer = solve(grid)
-        if answer is not None:
-            return answer
-    grid[x][y] = "."
-    return None
+        a = find_possible_values(grid, (x, y))
+        for i in a:
+            grid[x][y] = i
+            answer = solve(grid)
+            if answer is not None:
+                return answer
+        grid[x][y] = "."
+        return None
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
