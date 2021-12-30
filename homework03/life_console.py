9431260186eccd0aedb23f70edc06a71659082e4
mariaantonova334734 <mariaantonova2003@mail.ru>
2021-12-30 19:17:38 +0300

    black

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -1,12 +1,16 @@
 import curses
+import curses.ascii
+import pathlib
+import time
 
 from life import GameOfLife
 from ui import UI
 
 
 class Console(UI):
-    def __init__(self, life: GameOfLife) -> None:
+    def __init__(self, life: GameOfLife, saving: pathlib.Path) -> None:
         super().__init__(life)
+        self.saving = saving
 
     def draw_borders(self, screen) -> None:
         """Отобразить рамку."""
@@ -22,4 +26,37 @@ class Console(UI):
                     screen.addch(pos_in_row + 1, pos_in_col + 1, " ")
 
     def run(self) -> None:
-        curses.endwin()
+        curses.noecho()
+        screen.clear()
+        screen.refresh()
+        window = curses.newwin(self.life.rows + 2, self.life.cols + 2)
+        self.draw_borders(window)
+        window.timeout(1)
+        window.nodelay(True)
+        running = True
+        pause = False
+        while running:
+            screen = curses.initscr()
+            curses.noecho()
+            screen.clear()
+            screen.refresh()
+            window = curses.newwin(self.life.rows + 2, self.life.cols + 2)
+            self.draw_borders(window)
+            window.timeout(1)
+            window.nodelay(True)
+            running = True
+            pause = False
+            while running:
+                current_element = window.getch()
+                if current_element == ord("\n"):
+                    pause = False
+                elif current_element == ord("S"):
+                    self.life.save(self.saving)
+                elif current_element == curses.ascii.ESC:
+                    running = False
+                if not pause:
+                    self.draw_grid(window)
+                    window.refresh()
+                    self.life.step()
+                    time.sleep(2)
+            curses.endwin()
