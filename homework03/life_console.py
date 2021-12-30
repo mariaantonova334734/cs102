277eb15b2acd44c00cdf7de3ef908e0d4a1b92c3
mariaantonova334734 <mariaantonova2003@mail.ru>
2021-12-30 17:35:50 +0300

    first trial

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -21,31 +21,7 @@ class Console(UI):
                 else:
                     screen.addch(pos_in_row + 1, pos_in_col + 1, " ")
 
-    #def run(self) -> None:
-        screen = curses.initscr()
-        curses.noecho()
-        screen.clear()
-        screen.refresh()
-        window = curses.newwin(self.life.rows + 2, self.life.cols + 2)
-        self.draw_borders(window)
-        window.timeout(1)
-        window.nodelay(True)
+    def run(self) -> None:
 
-        running = True
-        paused = False
-        while running:
-            curr_el = window.getch()
-            if curr_el == ord("\n"):
-                paused = False if paused else True
-            elif curr_el == ord("S"):
-                self.life.save(self.save_path)
-            elif curr_el == curses.ascii.ESC:
-                running = False
-            if not paused:
-                self.draw_grid(window)
-                window.refresh()
-                self.life.step()
-
-                time.sleep(1)
 
         curses.endwin()
