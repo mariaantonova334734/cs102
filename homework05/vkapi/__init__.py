eb56880783694b0f3a3ee01486d9f9e5d323771c
Maria Antonova <90193542+mariaantonova334734@users.noreply.github.com>
2022-03-21 23:24:47 +0300

    Update __init__.py

--- homework05/vkapi/__init__.py
+++ homework05/vkapi/__init__.py
@@ -1,4 +1,4 @@
 from homework05.vkapi import config
-from homework05.vkapi.session import Session1
+from homework05.vkapi.session import Session
 
-session = Session1(config.VK_CONFIG["domain"])  # запуск сессии
+session = Session(config.VK_CONFIG["domain"])  # запуск сессии
