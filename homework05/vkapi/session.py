0880e2c20fef5147452d7ae546ac7e3370860ce3
Maria Antonova <90193542+mariaantonova334734@users.noreply.github.com>
2022-03-21 11:03:47 +0300

    Update session.py

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -6,7 +6,7 @@ from requests.adapters import HTTPAdapter  # type: ignore
 from requests.packages.urllib3.util.retry import Retry  # type: ignore
 
 
-class Session:
+class Session1:
     """
     Сессия.
 
@@ -63,6 +63,62 @@ class Session:
             self.base_url + "/" + url, timeout=self.timeout, *args, **kwargs
         )
         return response
+class Session:
+    """
+    Сессия.
+
+    :param base_url: Базовый адрес, на который будут выполняться запросы.
+    :param timeout: Максимальное время ожидания ответа от сервера.
+    :param max_retries: Максимальное число повторных запросов.
+    :param backoff_factor: Коэффициент экспоненциального нарастания задержки.
+    """
+    base_url: str
+    timeout: float
+    max_retries: int
+    backoff_factor: float
+    http: Retry
+    def __init__(
+        self,
+        base_url: str,
+        timeout: float = 5.0,
+        max_retries: int = 3,
+        backoff_factor: float = 0.3,
+    ) -> None:
+        self.base_url = base_url #передается аргумент base_url в параметр класса  self.base_url
+        self.timeout = timeout
+        self.session = requests.Session()
+        errors = []
+        for i in range(400, 600):
+            errors.append(i)
+
+        retry = Retry(
+            total=max_retries,
+            backoff_factor=backoff_factor,
+            status_forcelist=errors,
+            method_whitelist=["POST", "GET"]
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        self.session.mount("https://", adapter)
+
+    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+        response = self.session.get(
+            self.base_url + "/" + url, timeout=self.timeout, *args, **kwargs
+        )
+
+        return response
+
+    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+        response = self.session.post(
+            self.base_url + "/" + url, timeout=self.timeout, *args, **kwargs
+        )
+        return response
+
 
     #     self.http = requests.Session()
     #     self.http.mount("https://", adapter)
