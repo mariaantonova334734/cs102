0b709d1d0cd15a26b8540e884002f289f42b9f95
Maria Antonova <90193542+mariaantonova334734@users.noreply.github.com>
2022-03-21 23:18:11 +0300

    Update network.py

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -80,9 +80,7 @@ def describe_communities(
         for uid in cluster_users:
             for friend in friends:
                 if uid == friend["id"]:
-                    data.append(
-                        [cluster_n] + [friend.get(field) for field in fields]
-                    )  # type: ignore
+                    data.append([cluster_n] + [friend.get(field) for field in fields])  # type: ignore
                     break
     return pd.DataFrame(data=data, columns=["cluster"] + fields)
 
